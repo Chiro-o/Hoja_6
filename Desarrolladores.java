8ff3e2ebf00dad2c45544c5e0709949ca02bc9b5
Chiro1 <cris.a.06@hotmail.es>
2015-09-03 17:18:56 -0600

    Update Desarrolladores.java

--- Desarrolladores.java
+++ Desarrolladores.java
@@ -1,11 +1,20 @@
 import java.util.*;
 
+/**
+ * Universidad del valle de Guatemala
+ * @author Christopher Chiroy, 14411
+ * Desarrolladores, contiene los metodos que organizan a los tres tipos de desarrolladores
+ */
 public class Desarrolladores {
 	private Set<String> java;
 	private Set<String> web;
 	private Set<String> celular;
 	private Factory<String> tipo;
 	
+	/**
+	 * @param Seleccion
+	 * Constructor
+	 */
 	public Desarrolladores(int Seleccion){
 		tipo = new Factory<String>();
 		java = tipo.Set(Seleccion);
@@ -13,10 +22,20 @@ public class Desarrolladores {
 		celular = tipo.Set(Seleccion);
 	}
 	
+	/**
+	 * @return java
+	 */
 	public Set<String> getJava()     {return java;}
 	public Set<String> getWeb()      {return web;}
 	public Set<String> getCelular()  {return celular;}
 	
+	/**
+	 * @param nombre
+	 * @param j
+	 * @param w
+	 * @param c
+	 * Agrega un desarrollador a al grupo correspondiente
+	 */
 	public void agregrDesarrollador(String nombre, int j, int w, int c){
 		if(j==1)
 			java.add(nombre);
@@ -26,6 +45,9 @@ public class Desarrolladores {
 			celular.add(nombre);
 	}
 	
+	/**
+	 * Muestra el listado de las personas que son desarrolladores java, web y celulares
+	 */
 	public void JWC(){
 		Iterator<String> condicion = java.iterator();
 		Set<String> cumplen = new HashSet<String>();
@@ -42,6 +64,9 @@ public class Desarrolladores {
 		}
 	}
 	
+	/**
+	 * Muestra el listado de las personas que son desarrolladores web pero no java
+	 */
 	public void JnoW(){
 		Iterator<String> condicion = java.iterator();
 		Set<String> cumplen = new HashSet<String>();
@@ -58,6 +83,9 @@ public class Desarrolladores {
 		}
 	}
 	
+	/**
+	 * Muestra el listado de las personas que son desarrolladores web y celular pero no java
+	 */
 	public void WyCnoJ(){
 		Iterator<String> condicion = java.iterator();
 		Set<String> cumplen = new HashSet<String>();
@@ -74,6 +102,9 @@ public class Desarrolladores {
 		}
 	}
 	
+	/**
+	 * Muestra el listado de las personas que son desarrolladores web o celular pero no java
+	 */
 	public void WoC(){
 		Iterator<String> condicion = java.iterator();
 		Set<String> cumplen = new HashSet<String>();
@@ -90,6 +121,9 @@ public class Desarrolladores {
 		}
 	}
 	
+	/**
+	 * Verifica si el conjunto de desarrolladores java es un subconjunto de desarrolladores web
+	 */
 	public void javaSubWeb(){
 		Iterator<String> condicion = java.iterator();
 		Set<String> cumplen = new HashSet<String>();
@@ -105,6 +139,9 @@ public class Desarrolladores {
 			System.out.println("Los desarrolladores de Java NO son un subConjunto de los desarrolladores web");
 	}
 	
+	/**
+	 * @return El Conjunto con mayor cantidad de desarrolladores
+	 */
 	public Set<String> conjuntoMayor(){
 		if(java.size()>web.size() && java.size()>celular.size()){
 			System.out.println("El conjunto con mayor cantidad de desarrolladores es Java y los integrantes son:");
@@ -120,6 +157,9 @@ public class Desarrolladores {
 		}
 	}
 	
+	/**
+	 * Devuelve el conjunto con mayor cantidad de desarrolladores desordenado
+	 */
 	public void mayorDesordenado(){
 		Iterator<String> Lista = conjuntoMayor().iterator();
 		while(Lista.hasNext()){
@@ -127,6 +167,9 @@ public class Desarrolladores {
 		}
 	}
 	
+	/**
+	 * Devuelve el conjunto con mayor cantidad de desarrolladores oedenado
+	 */
 	public void mayorOrdenado(){
 		Iterator<String> desorden = conjuntoMayor().iterator();
 		Set<String> orden = new TreeSet<String>();
@@ -140,7 +183,6 @@ public class Desarrolladores {
 			System.out.println(Lista.next());
 		
 		}
-		
 	}
 	
 }
